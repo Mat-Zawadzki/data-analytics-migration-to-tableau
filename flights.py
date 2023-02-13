9baeefac39d33a9efbf20b4e14a240f2fd86d084
M Zawadzki <114954374+ThePirateLegend@users.noreply.github.com>
2023-02-13 23:13:17 +0000

    Update flights.py

--- flights.py
+++ flights.py
@@ -15,14 +15,12 @@ class cleanFiles():
         self.combined_data_frame = pd.DataFrame()
 
 
-
     def get_list_of_files(self):
         self.list_of_files = [f for f in listdir('F://csvfiles')]
         for file_path in range(len(self.list_of_files)):
             self.list_of_files[file_path] = ("F://csvfiles//" + self.list_of_files[file_path])
         self.list_of_files.sort()
         #print(self.list_of_files)
-
 
 
     def get_list_of_years(self):
@@ -33,13 +31,11 @@ class cleanFiles():
         print(self.list_of_years)
 
 
-
     def get_names_for_dataframes(self):
         '''Appends the list_of_dataframes with names for them to be changed into dataframes  '''
         for file_path in range(len(self.list_of_files)):
             dataframe_name = "pd_read_" + self.list_of_years[file_path]
             self.list_of_dataframes.append(dataframe_name)
-
 
 
     def create_dataframes_for_files(self):
@@ -48,7 +44,6 @@ class cleanFiles():
         for num in range(len(self.list_of_dataframes)):
             self.list_of_dataframes[num] = pd.read_csv(self.list_of_files[num])
             #print(self.list_of_dataframes)
-
 
 
     '''Kept this function for individual dataframe cleaning and testing'''
@@ -69,7 +64,6 @@ class cleanFiles():
             #print(len(dataframe.axes[1]))'''
 
 
-
     def set_indexes_for_dataframes(self):
         '''Sets an index column (Value) for the dataframes to be concated on'''
         for dataframe in self.list_of_dataframes:
@@ -77,7 +71,6 @@ class cleanFiles():
             dataframe["Value"] = range(self.how_many_rows_in_all_dataframes+1, len(dataframe.axes[0])+self.how_many_rows_in_all_dataframes+1)
             self.how_many_rows_in_all_dataframes = self.how_many_rows_in_all_dataframes + len(dataframe.axes[0])
             dataframe.set_index("Value", inplace = True)
-
 
 
     def create_and_clean_combined_dataframe(self):
@@ -89,7 +82,6 @@ class cleanFiles():
                     self.combined_data_frame = self.combined_data_frame.drop([col], axis = 1)
                 else:
                     self.combined_data_frame[col] = self.combined_data_frame[col].fillna(0)
-
 
 
     def combined_dataframe_to_csv(self):
@@ -105,7 +97,7 @@ def main():
     cf.set_indexes_for_dataframes()
     cf.create_and_clean_combined_dataframe()
     cf.combined_dataframe_to_csv()
-
+    
 
 
 if __name__ == "__main__":
